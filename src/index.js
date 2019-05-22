64bb31aa2d7ba9b15aace8ac077581e9ead0f3d9
Rafael Souza <rafaelangical2@gmail.com>
2019-05-21 21:35:35 -0300

    Add index.js configured with store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap';
+import createSagaMiddleware from 'redux-saga';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+import reducer from './reducers';
+import App from './components/App';
+import rootSaga from './sagas';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(
+   reducer,
+   applyMiddleware(sagaMiddleware, logger),
+
+);
+
+sagaMiddleware.run(rootSaga);
+
+render(
+   <Provider store={store}>
+     <App />
+   </Provider>,
+document.getElementById('root'),
+);
