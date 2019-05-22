149265806c6ca82e9a32c43f4731b90380ca55cb
Rafael Souza <rafaelangical2@gmail.com>
2019-05-21 21:38:49 -0300

    Add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,55 @@
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+
+      //ALL CHARACTERS
+    case 'GET_CHARACTERS':
+      console.log('GET_CHARACTERS REDUCER');
+      return { ...state, loading: true };
+
+      //CHARACTERS RECEIVED
+    case 'CHARACTERS_RECEIVED':
+      console.log('CHARACTERS_RECEIVED REDUCER');
+      return { ...state, characters: action.characters, loading: false }
+    
+      //FETCHING CHARACTERS
+    case 'FETCHING_CHARACTERS':
+      console.log('FETCHING_CHARACTERS REDUCER');
+      console.log('fetching characters data');
+      return { ...state, loading: true }
+    
+      //ERROR ON GET CHARACTERS
+    case 'CHARACTERS_FAILED':
+      console.log('FETCHING_CHARACTERS_FAILED REDUCER');
+      return { ...state, loading: false }
+
+    ///////////////
+
+
+      // GET CHARACTER BY ID  
+    case 'GET_CHARACTER_BY_ID': 
+      console.log('GET_CHARACTER_BY_ID REDUCER');
+      return { ...state, loadind: true }
+      //FETCHING CHARACTERS BY ID
+    case 'FETCHING_CHARACTER_BY_ID': 
+      console.log('FETCHING_CHARACTER_BY_ID REDUCER');
+      return { ...state, loadind: true }
+      
+      //CHARACTERS RECEIVED
+    case 'CHARACTER_BY_ID_RECEIVED': 
+      console.log('CHARACTER_BY_ID REDUCER');
+      console.log(state);
+      return { ...state, loading: false }
+      
+      //CHARACTERS GET FAILED 
+    case 'GET_CHARACTER_BY_ID_FAILED':
+      console.log('GET_CHARACTER_BY_ID_FAILED REDUCER');
+      return { ...state }
+
+      ///////////////////
+      
+      //DEFAULT PAYLODAD
+    default: 
+      return state;
+  }
+ };
+ export default reducer;
