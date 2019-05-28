2addf232f911a700da264a0826f80638f8bffdbc
Rafael Souza <rafaelangical2@gmail.com>
2019-05-28 18:32:36 -0300

    Update modal characterItem

--- src/containers/CharacterItem.js
+++ src/containers/CharacterItem.js
@@ -6,13 +6,19 @@ import { closeModal } from '../actions';
 
 let CharacterItem = ({ characterById, closeModal }) => (
   characterById ?
-  <Modal.Dialog aria-labelledby="contained-modal-title-vcenter" centered style={{ justifySelf: 'center', alignSelf: 'center', marginTop: '0 auto', padding: 0, position: 'absolute', top: 0, left: 0, right: 0, bottom:0 }} >
+  <Modal.Dialog aria-labelledby="contained-modal-title-vcenter" centered style={{ justifySelf: 'center', alignSelf: 'center', marginTop: '0 auto', padding: 0, position: 'fixed', top: 0, left: 0, right: 0, bottom:0 }} >
     <Modal.Header closeButton={false}>
-      <Modal.Title>{characterById.name}</Modal.Title>
+      <Modal.Title style={{ color: 'darkorange', width: '90%', margin: '0 auto'}}>{characterById.name}</Modal.Title>
     </Modal.Header>
 
-    <Modal.Body>
+    <Modal.Body style={{ width: '90%', margin: '0 auto'}}>
       <p>{characterById.description !== '' ? characterById.description : 'No Description'}</p>
+      <p style={{color: 'darkblue'}}>Comics:</p>
+      {characterById.comics.items.slice(0,5).map((item,id) => {
+        return(
+          <ul key={id}><li>{item.name !== '' ? item.name : 'No comics'}</li></ul>
+        );
+      })}
     </Modal.Body>
 
     <Modal.Footer>
