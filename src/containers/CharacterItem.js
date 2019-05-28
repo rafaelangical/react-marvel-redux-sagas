9dcec5c310483d3455c3f56bdccc844a41fe27d6
Rafael Souza <rafaelangical2@gmail.com>
2019-05-27 22:23:47 -0300

    Update

--- src/containers/CharacterItem.js
+++ src/containers/CharacterItem.js
@@ -6,7 +6,7 @@ import { closeModal } from '../actions';
 
 let CharacterItem = ({ characterById, closeModal }) => (
   characterById ?
-  <Modal.Dialog style={{ width: '100vw', height: '100vh', justifySelf: 'center', alignSelf: 'center', marginTop: '40vh', padding: 0, position: 'absolute', top: 0, left: 0, right: 0, bottom:0 }} >
+  <Modal.Dialog aria-labelledby="contained-modal-title-vcenter" centered style={{ justifySelf: 'center', alignSelf: 'center', marginTop: '0 auto', padding: 0, position: 'absolute', top: 0, left: 0, right: 0, bottom:0 }} >
     <Modal.Header closeButton={false}>
       <Modal.Title>Modal title</Modal.Title>
     </Modal.Header>
